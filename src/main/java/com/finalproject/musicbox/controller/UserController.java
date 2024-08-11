a1eff0c997eb54578a87854aad816bce77880041
dci-student <glaventzakis@gmail.com>
2024-08-11 11:46:48 +0200

    new updateds

--- src/main/java/com/finalproject/musicbox/controller/UserController.java
+++ src/main/java/com/finalproject/musicbox/controller/UserController.java
@@ -1,4 +1,6 @@
 package com.finalproject.musicbox.controller;
 
 public class UserController {
+    // Controller class for handling user-related requests.
+    // This class will define the endpoints for user registration, login, and other user-related operations.
 }
--- src/main/java/com/finalproject/musicbox/repository/RoleRepository.java
+++ src/main/java/com/finalproject/musicbox/repository/RoleRepository.java
@@ -1,4 +1,7 @@
 package com.finalproject.musicbox.repository;
 
+import com.finalproject.musicbox.model.Role;
+
 public interface RoleRepository {
+    Role findByName(String name);
 }
