7130bcbab4734502ef5ea855fbc20c3bca9d28e5
dci-student <glaventzakis@gmail.com>
2024-08-11 11:50:21 +0200

    new updateds

--- src/main/java/com/finalproject/musicbox/controller/UserController.java
+++ src/main/java/com/finalproject/musicbox/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.finalproject.musicbox.controller;
 
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@Validated
 public class UserController {
     // Controller class for handling user-related requests.
     // This class will define the endpoints for user registration, login, and other user-related operations.
